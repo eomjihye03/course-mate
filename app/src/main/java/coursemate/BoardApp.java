619365c91f0871912996616a1963614ab08154dc
eomjihye03 <jina.jihye.eom@gmail.com>
2025-08-11 21:26:47 +0900

    gradle build 도구 적용.

--- app/src/main/java/coursemate/BoardApp.java
+++ app/src/main/java/coursemate/BoardApp.java
@@ -1,3 +1,10 @@
+/**
+ * BoardApp.java 게시글 관리 시스템의 메인 클래스입니다. 프로그램 실행 시 환영 메시지를 출력합니다.
+ *
+ * @author 작성자명
+ * @version 1.0
+ * @since 2025-08-11
+ */
 public class BoardApp {
   public static void main(String[] args) {
     System.out.println("[게시글 관리 시스템]");
