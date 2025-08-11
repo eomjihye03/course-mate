97cd10d956d090a59cda94d27f1b15e6a501bf1e
eomjinyoung <jinyoung.eom@gmail.com>
2025-08-11 16:56:12 +0900

    main-1

--- MemberApp.java
+++ MemberApp.java
@@ -2,5 +2,6 @@ public class MemberApp {
   public static void main(String[] args) {
     System.out.println("[회원관리 시스템]");
     System.out.println("회원관리 시스템에 오신 것을 환영합니다.");
+    System.out.println("이 시스템은 회원 정보를 관리하는 데 사용됩니다.");
   }
 }
