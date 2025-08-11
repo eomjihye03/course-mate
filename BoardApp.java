836549ba5cd85b58d03831e8bdb3b979f4931211
eomjihye03 <jina.jihye.eom@gmail.com>
2025-08-11 16:56:15 +0900

    main-1

--- BoardApp.java
+++ BoardApp.java
@@ -2,5 +2,6 @@ public class BoardApp {
   public static void main(String[] args) {
     System.out.println("[게시글 관리 시스템]");
     System.out.println("보드 관리 시스템에 오신 걸 환영합니다.");
+    System.out.println("=================================");
   }
 }
