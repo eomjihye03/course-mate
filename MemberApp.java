b49c45058d91411c0103d5898261d4d5f82f9db0
eomjinyoung <jinyoung.eom@gmail.com>
2025-08-11 17:02:20 +0900

    b2-2

--- MemberApp.java
+++ MemberApp.java
@@ -3,5 +3,6 @@ public class MemberApp {
     System.out.println("[회원관리 시스템]");
     System.out.println("회원관리 시스템에 오신 것을 환영합니다.");
     System.out.println("b2-1");
+    System.out.println("b2-2");
   }
 }
