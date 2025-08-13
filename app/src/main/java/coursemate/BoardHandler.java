8241f7f10a88ff0138f6932b1ec2f93953f2f50d
eomjihye03 <jina.jihye.eom@gmail.com>
2025-08-13 20:32:22 +0900

    BoardApp 수정

--- app/src/main/java/coursemate/BoardHandler.java
+++ app/src/main/java/coursemate/BoardHandler.java
@@ -11,7 +11,7 @@ public class BoardHandler implements MenuHandler {
 
   public void execute() { // instance method가 되어야함. public을 생략하면 pakage 멤버가 됨.
     System.out.println("[게시글 관리 시스템]");
-    System.out.println("보드 관리 시스템에 오신 걸 환영합니다.");
+    System.out.println("게시글 관리 시스템에 오신 걸 환영합니다.");
     printMainMenu();
 
     label:
@@ -21,19 +21,19 @@ public class BoardHandler implements MenuHandler {
 
       switch (input) {
         case "1":
-          System.out.println("회원 추가입니다.");
+          System.out.println("게시글 추가입니다.");
           break;
         case "2":
-          System.out.println("회원 목록입니다.");
+          System.out.println("게시글 목록입니다.");
           break;
         case "3":
-          System.out.println("회원 조회입니다.");
+          System.out.println("게시글 조회입니다.");
           break;
         case "4":
-          System.out.println("회원 변경입니다.");
+          System.out.println("게시글 변경입니다.");
           break;
         case "5":
-          System.out.println("회원 삭제입니다.");
+          System.out.println("게시글 삭제입니다.");
           break;
         case "0":
           break label;
@@ -48,11 +48,11 @@ public class BoardHandler implements MenuHandler {
   }
 
   static void printMainMenu() {
-    System.out.println("1. 회원 추가");
-    System.out.println("2. 회원 목록");
-    System.out.println("3. 회원 조회");
-    System.out.println("4. 회원 변경");
-    System.out.println("5. 회원 삭제");
+    System.out.println("1. 게시글 추가");
+    System.out.println("2. 게시글 목록");
+    System.out.println("3. 게시글 조회");
+    System.out.println("4. 게시글 변경");
+    System.out.println("5. 게시글 삭제");
     System.out.println("0. 메인");
   }
 }
