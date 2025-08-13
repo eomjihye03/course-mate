a172ad9641942373549e995bf1f61a9a03ab953c
eomjihye03 <jina.jihye.eom@gmail.com>
2025-08-13 17:29:15 +0900

    BoardApp 수정

--- app/src/main/java/coursemate/BoardApp.java
+++ app/src/main/java/coursemate/BoardApp.java
@@ -1,14 +1,7 @@
 package coursemate;
 
-/**
- * BoardApp.java 게시글 관리 시스템의 메인 클래스입니다. 프로그램 실행 시 환영 메시지를 출력합니다.
- *
- * @author 작성자명
- * @version 1.0
- * @since 2025-08-11
- */
 public class BoardApp {
-  public static void main(String[] args) {
+  static void service() {
     System.out.println("[게시글 관리 시스템]");
     System.out.println("보드 관리 시스템에 오신 걸 환영합니다.");
   }
